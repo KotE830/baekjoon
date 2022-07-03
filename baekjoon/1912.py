f8c4ca43b6eb1b8f3b81139d21601ad226d023ea
KotE830 <84309149+KotE830@users.noreply.github.com>
2022-07-03 02:20:52 +0000

    modify 1912

--- baekjoon/1912.py
+++ baekjoon/1912.py
@@ -1,35 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 nums = list(map(int, input().split()))
 
-sums = 0
-li = []
-for i in nums:
-    if i > 0:
-        sums = sums + i
-    elif i < 0:
-        if sums > 0:
-            li.append(sums)
-        li.append(i)
-        sums = 0
-if sums > 0:
-    li.append(sums)
+for i in range(1, len(nums)):
+    if nums[i-1] > 0:
+        nums[i] = nums[i] + nums[i-1]
 
-result = li[0]
-min, max = 0, 0
-print(li)
-
-for i in range(len(li)):
-    print(sum(li[:i+1]))
-    if li[i] > 0 and result < sum(li[:i+1]):
-        result = sum(li[:i+1])
-        print(result, "I")
-        max = i+1
-        
-for i in range(max):
-    print(sum(li[i:max]))
-    if li[i] > 0 and result < sum(li[i:max]):
-        result = sum(li[i:max])
-        print(result, "I")
-        min = i
-
-print(result)
+print(max(nums))
