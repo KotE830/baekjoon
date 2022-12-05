71f41d2e6247c7f5143869dff819f5292a7c574d
KotE830 <84309149+KotE830@users.noreply.github.com>
2022-12-05 09:51:03 +0000

    Change folder's name as 'python'

--- main.py
+++ main.py
@@ -2,48 +2,48 @@ from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
-class TrieNode:
-    def __init__(self):
-        self.word = False
-        self.children = defaultdict(TrieNode)
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        self.names = {}
-
-    # insert color in trie
-    def insert_color(self, color: str) -> None:
-        node = self.root
-        for char in color:
-            node = node.children[char]
-        node.word = True
-
-    def insert_name(self, name: str) -> None:
-        self.names[name] = 0
+# find the greatest weight
+def max_weight(N, islands, start, end, max_C) -> int:
+    # check if start and end are connected with w
+    def connected(weight: int) -> bool:
+        visited = [False for _ in range(N+1)]
+        stack = [start]
+        visited[start] = True
         
-    # check if the team is in the trie
-    def search(self, team: str) -> bool:
-        # check if the color is in the trie
-        node = self.root
-        for index, char in enumerate(team):
-            if char not in node.children:
-                return False
-            node = node.children[char]
-            # check if the name is in the dictionary
-            if node.word and team[index+1:] in self.names:
-                    return True
+        while stack:
+            x = stack.pop()
+            for v, w in islands[x]:
+                if not visited[v] and weight <= w:
+                    if v == end:
+                        return True
+                        
+                    visited[v] = True
+                    stack.append(v)
+
+        return False
         
-            
-C, N = map(int, input().split())
-trie = Trie()
-for _ in range(C):
-    trie.insert_color(input().rstrip())
-for _ in range(N):
-    trie.insert_name(input().rstrip())
+
+    left, right = 1, max_C
+    while left <= right:
+        mid = left + (right - left) // 2
+        if connected(mid):
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left-1
     
-for _ in range(int(input())):
-    if trie.search(input().rstrip()):
-        print("Yes")
-    else:
-        print("No")
+
+N, M = map(int, input().split())
+max_C = 0
+
+islands = defaultdict(list)
+for _ in range(M):
+    A, B, C = map(int, input().split())
+    islands[A].append((B, C))
+    islands[B].append((A, C))
+    max_C = max(max_C, C)
+
+start, end = map(int, input().split())
+
+print(max_weight(N, islands, start, end, max_C))
